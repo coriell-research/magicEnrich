41fc27e5f5e8353caf899d604e87bb7982221d17
jcalendo <jcalendo@gmail.com>
2020-02-11 14:43:02 -0500

    untested final version

--- magicRE_count.py
+++ magicRE_count.py
@@ -9,10 +9,9 @@ from collections import defaultdict
 from pathlib import Path
 
 
-def align_to_pseudogenome(pseudogenome_db, sample_name, read_type, read_handle, read_handle2, threads, paired_end, debug):
+def align_to_pseudogenome(outdir, pseudogenome_db, sample_name, read_type, read_handle, read_handle2, threads, paired_end):
     """Align reads to the pseudogenome using magicBLAST"""
-    tmp_dir = Path(Path(pseudogenome_db).parent, "__{sample_name}_tmp"))
-    outfile = Path(tmp_dir, f"__{sample_name}_magicBLAST_results.tab")
+    outfile = Path(out_dir, f"__{sample_name}_magicBLAST_results.tab")
 
     if read_type == "sra":
         align_cmd = f"magicblast -sra {read_handle} -db {pseudogenome_db} -num_threads {threads} -outfmt tabular -no_unaligned -splice F -limit_lookup F > {outfile}"
@@ -32,8 +31,7 @@ def align_to_pseudogenome(pseudogenome_db, sample_name, read_type, read_handle, 
     # run the alignment
     subprocess.run(shlex.split(align_cmd), shell = True)
 
-    if not debug:
-        subprocess.run(f"rm -r {tmp_dir}")
+    return outfile
 
 
 def combine_counts(counts1, counts2):
@@ -103,17 +101,17 @@ def create_element_mapping(repnames_bedfile):
     return elem_key
 
 
-def map_counts(multi_counts, element_mapper):
+def map_counts(counts, element_mapper):
     """Create a dictionary of reads using the class > family > element hierarchy.
     Since the output from the alignment step function returns element names and their counts, 
     these element names must be re-associated with the classes and families they are part of. 
     This association is performed by the element_mapper.
     """
     hierarchy = defaultdict(lambda : defaultdict(lambda : defaultdict(float)))
-    for elem in multi_counts.keys():
+    for elem in counts.keys():
         class_ = element_mapper[elem]['class']
         family = element_mapper[elem]['family']
-        hierarchy[class_][family][elem] += multi_counts[elem]
+        hierarchy[class_][family][elem] += counts[elem]
 
     return hierarchy
 
@@ -157,18 +155,82 @@ def write_family_summary(family_counts, outfile):
                 f.write(f"{class_}\t{family}\t{count}\n")
 
 
+def write_output(count_data, outfile):
+    """Write count data to the outfile. Output truncates floats to ints."""
+    print(f"Writing results to {outfile}...")
+    with open(outfile, "w") as f:
+        for class_ in count_data:
+            for family in count_data[class_]:
+                for elem in count_data[class_][family]:
+                    count = int(count_data[class_][family][elem])
+                    f.write(f"{class_}\t{family}\t{elem}\t{count}\n")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Align reads to pseudogenome using magicBLAST and return counts of repeat elements")
     parser.add_argument('sampleName', help='The name of the sample to be processed.')
     parser.add_argument('setupDir', help="Path to the magicRE_Setup directory.")
-    parser.add_argument('readType', help='Defines the input format to be aligned. One of [SRA, fasta, fastq]. Defines the input format to be aligned.', choices=['SRA', 'fasta', 'fastq'])
+    parser.add_argument('queryType', default='fastq', choices=['sra', 'fasta', 'fastq'], help='Defines the input format to be aligned. One of [SRA, fasta, fastq]. Defines the input format to be aligned.')
+    parser.add_argument('query', help="Either the SRA accession number or the path to the fasta or fastq file to be aligned to the pseudogenome.")
+    parser.add_argument('--query2', default=None, help="If pairedEnd is set, designate this to be the path to second read pair.")
     parser.add_argument('--percentIdentity', default=90, type=float, help='Threshold at which to include mapped reads in the count data. Mapped reads must be >= percentIdentity to be included.')
     parser.add_argument('--threads', default=1, type=int, help='Number of threads to use for alignment.')
     parser.add_argument('--pairedEnd', dest='pairedEnd', action='store_true', help='Designate this option for paired-end sequencing.')
     parser.add_argument('--summarize', dest='summarize', action='store_true', help='In addition to the repeat name level output, produce collapsed counts at the class and family levels for each of the count types.')
-    parser.add_argument('--debug', dest='debug', action='store_true', help='Select this option to prevent the removal of temporary files; useful for debugging')
+    parser.add_argument('--outDirectory', default=".", help="Specify where to save the count results. Defaults to the current directory/sampleName.")
+    parser.add_argument('--debug', dest='debug', action='store_true', help='Select this option to prevent the removal of temporary files; useful for debugging.')
     parser.set_defaults(pairedEnd=False, summarize=False, debug=False)
     parser.parse_args()
+
+    sample_name = args.sampleName
+    setup_dir = args.setupDir
+    query_type = args.queryType
+    query = args.query
+    query2 = args.query2
+    perc_id = args.percentIdentity
+    threads = args.threads
+    paired_end = args.pairedEnd
+    summarize_data = args.summarize
+    out_path = arg.outDirectory
+    debug = args.debug
+
+    # setup file i/o ----------------------------------------------------------
+    out_dir = Path(out_path, sample_name)
+    pseudogenome_db = Path(setup_dir, "pseudogenome")
+    repnames_bedfile = Path(setup_dir, "repnames.bed")
+    total_counts_outfile = Path(out_dir, sample_name + "_total_counts.tsv")
+    uniq_counts_outfile = Path(out_dir, sample_name + "_unique_counts.tsv")
+    fractional_counts_outfile = Path(out_dir, sample_name + "_fractional_counts.tsv")
+    class_total_counts_outfile = Path(out_dir, sample_name + "_class_total_counts.tsv")
+    class_uniq_counts_outfile = Path(out_dir, sample_name + "_class_unique_counts.tsv")
+    class_fractional_counts_outfile = Path(out_dir, sample_name + "_class_fractional_counts.tsv")
+    family_total_counts_outfile = Path(out_dir, sample_name + "_family_total_counts.tsv")
+    family_uniq_counts_outfile = Path(out_dir, sample_name + "_family_unique_counts.tsv")
+    family_fractional_counts_outfile = Path(out_dir, sample_name + "_family_fractional_counts.tsv")
+
+    # main routine ------------------------------------------------------------
+    res_path = align_to_pseudogenome(out_dir, pseudogenome_db, sample_name, query_type, query, query2, threads, paired_end)
+    raw_uniq, raw_frac, raw_total = count_magicBLAST_result(res_path, perc_id)
+    elem_mapper = create_element_mapping(repnames_bedfile)
+    uniq_counts = map_counts(raw_uniq, elem_mapper)
+    frac_counts = map_counts(raw_frac, elem_mapper)
+    total_counts = map_counts(raw_total, elem_mapper)
+    fractional_counts = combine_counts(uniq_counts, frac_counts)  # fractional counts must be combined with unique counts before writing out
+    write_output(fractional_counts, fractional_counts_outfile)
+    write_output(total_counts, total_counts_outfile)
+    write_output(uniq_counts, uniq_counts_outfile)
+
+    if summarize_data:
+        print("Writing class-level and family-level summarized counts...")
+        class_total, family_total = summarize(total_counts)
+        class_uniq, family_uniq = summarize(uniq_counts)
+        class_fractional, family_fractional = summarize(fractional_counts)
+        write_class_summary(class_total, class_total_counts_outfile)
+        write_class_summary(class_uniq, class_uniq_counts_outfile)
+        write_class_summary(class_fractional, class_fractional_counts_outfile)
+        write_family_summary(family_total, family_total_counts_outfile)
+        write_family_summary(family_uniq, family_uniq_counts_outfile)
+        write_family_summary(family_fractional, family_fractional_counts_outfile)
 
 
 if __name__ == '__main__':
